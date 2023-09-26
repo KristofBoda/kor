9b15a539ebaae450b33354b8057c65fd0f83168c
KristofBoda <krristof100@gmail.com>
2023-09-26 12:57:12 +0200

    Add project files.

---
+++ kor1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Példányosítom az osztályt
+            KorClass kor1 = new KorClass(3.0);
+            kor1.setKerulet(kor1.getSugar());
+            kor1.setTerulet();
+            Console.WriteLine("A {0: 0.0000} sugarú kör kerülete: {1: 0.0000}, terulete: {2: 0.0000}", kor1.getSugar(), kor1.getKerulet(), kor1.getTerulet());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ korunittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("korunittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("korunittest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c564958-a908-4fc5-b6c9-0322fb96de42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
